13090436494afefd264d09ee94030698d7dd617b
Mark Norton <mrsbadcrumble@gmail.com>
2020-01-28 21:18:27 +0000

    Test coverage for problem 9

--- problems/triplet.test.js
+++ problems/triplet.test.js
@@ -1,8 +1,15 @@
-const { Triplet } = require('./triplet');
+const { Triplet, Problem9 } = require('./triplet');
 
 test('the CORRECT TRIPLET is returned for the desired SUM', () => {
     const t = new Triplet();
-    const answer = t.sumTo(12);
+    let answer = t.sumTo(12);
+    expect(answer).toBeDefined();
+    expect(answer.a).toBe(3);
+    expect(answer.b).toBe(4);
+    expect(answer.c).toBe(5);
+
+    const p = new Problem9();
+    answer = p.solve(12);
     expect(answer).toBeDefined();
     expect(answer.a).toBe(3);
     expect(answer.b).toBe(4);
